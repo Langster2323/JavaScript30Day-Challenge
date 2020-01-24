af484801390f1bd6efba5c2158dacb06cbaba4a7
Langster2323 <langahkeem@gmail.com>
2020-01-23 20:07:29 -0600

    Fixed skip issue

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,7 +4,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelector('.player__slider');
 
 /* Build out functions */
@@ -20,6 +20,10 @@ function updateButton() {
     console.log('update button');
 }
 
+function skip() {
+    console.log('Skip');
+}
+
 /* Hook up the event listners */
 
 video.addEventListener('click', togglePlay);
@@ -27,3 +31,4 @@ video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
