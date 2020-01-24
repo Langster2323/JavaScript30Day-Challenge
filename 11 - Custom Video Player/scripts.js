aaba9f347acdacd4a479cb2d55ee74df6732bc76
Langster2323 <langahkeem@gmail.com>
2020-01-23 20:15:57 -0600

    Updating ranges input

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,7 +5,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 /* Build out functions */
 
@@ -22,7 +22,13 @@ function updateButton() {
 
 function skip() {
     console.log(this.dataset.skip);
+    console.log(video.currentTime);
     video.currentTime += parseFloat(this.dataset.skip)
+}
+
+function handleRangeUpdate() {
+    console.log(this.value);
+
 }
 
 /* Hook up the event listners */
@@ -32,4 +38,6 @@ video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
